ec16f8c3b0d36d36e065ad70b9ea0b8b7fc271cc
JanThorwarth <thorwarth.jan@web.de>
2024-09-12 16:55:07 +0200

    added above the fold section

---
+++ src/app/abovethefold/abovethefold.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-abovethefold',
+  standalone: true,
+  imports: [],
+  templateUrl: './abovethefold.component.html',
+  styleUrl: './abovethefold.component.scss'
+})
+export class AbovethefoldComponent {
+
+}
