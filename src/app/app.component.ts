0cb1ee4785c6aa3fbd37a11bd39b18c1382b4abf
JanThorwarth <thorwarth.jan@web.de>
2024-09-19 16:17:24 +0200

    added about me and started skills section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { AbovethefoldComponent } from './abovethefold/abovethefold.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { SkillsComponent } from './skills/skills.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, AbovethefoldComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, AbovethefoldComponent, AboutmeComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
